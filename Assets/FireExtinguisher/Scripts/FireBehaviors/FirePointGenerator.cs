9cfc49aded5547a489ba92859bac75f5cd91a59d
MitulChhipa <mitulchhipa5735@gmail.com>
2024-04-02 16:30:50 +0530

    improved fire point generation for plane prefabs

--- Assets/FireExtinguisher/Scripts/FireBehaviors/FirePointGenerator.cs
+++ Assets/FireExtinguisher/Scripts/FireBehaviors/FirePointGenerator.cs
@@ -14,27 +14,35 @@ namespace FireExtinguisher.Fire
         private int _gridSizeX;
         private int _gridSizeY;
 
+        public GameObject _firePointPrefab;
+        public List<FirePoint> _firePoints = new List<FirePoint>();
 
         public void PlacePoints(GameObject pointObject, ref List<FirePoint> firePoints)
         {
             int count = 0;
+            Bounds bounds = _renderer.bounds;
 
-            _gridSizeX = (int)(_scalerTransform.lossyScale.x / cellSize.x);
-            _gridSizeY = (int)(_scalerTransform.lossyScale.y / cellSize.y);
 
+            _gridSizeX = (int)(bounds.extents.x * 2 / cellSize.x);
+            _gridSizeY = (int)(bounds.extents.z * 2 / cellSize.y);
+
+            Vector3 center = bounds.center;
 
             Vector3 position = new Vector3();
 
-            position.x = (-_scalerTransform.lossyScale.x / 2) + cellSize.x / 2;
+            position.x = (-bounds.extents.x) + cellSize.x / 2;
+
+            if (_gridSizeX == 0) { _gridSizeX = 1; }
+            if (_gridSizeY == 0) { _gridSizeY = 1; }
 
             for (int x = 0; x < _gridSizeX; x++)
             {
-                position.z = (-_scalerTransform.lossyScale.y / 2) + +cellSize.y / 2;
+                position.z = (-bounds.extents.z * 2 / 2) + cellSize.y / 2;
 
                 for (int y = 0; y < _gridSizeY; y++)
                 {
                     GameObject go = Instantiate(pointObject, _firePointsParent);
-                    go.transform.position = position + _firePointsParent.position;
+                    go.transform.position = position + center;
                     go.transform.localRotation = Quaternion.identity;
 
                     firePoints.Add(go.GetComponent<FirePoint>());
@@ -43,15 +51,6 @@ namespace FireExtinguisher.Fire
                     count++;
                 }
                 position.x += cellSize.x;
-            }
-
-            if (count == 0)
-            {
-                GameObject go = Instantiate(pointObject, _firePointsParent);
-                go.transform.position = position;
-                go.transform.localRotation = Quaternion.identity;
-
-                firePoints.Add(go.GetComponent<FirePoint>());
             }
         }
     }
